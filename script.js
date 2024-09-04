06961d7f88ea9b1aef911e61962b2752470747fa
LVCSRer2 <ironicalx@gmail.com>
2024-09-04 23:44:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function audioProcess(event) {
 document.getElementById('startRecording').addEventListener('click', async () => {
     await initializeAudio();
 
-    webSocket = new WebSocket('wss://13.124.62.252:8000/ws/audio');
+    webSocket = new WebSocket('ws://13.124.62.252:8000/ws/audio');
     
     webSocket.onopen = () => {
         document.getElementById('startRecording').disabled = true;
